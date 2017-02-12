349799f4d19961cf6fe49f18f0a2a01f27875d19
XanderPSON <xwpeterson@gmail.com>
2017-02-11 23:54:17 -0800

    Latest update from Xander

--- index.js
+++ index.js
@@ -2,63 +2,57 @@ var flock = require('flockos');
 var config = require('./config.js');
 var express = require('express');
 var fs = require('fs');
-
-var users = [
-  {
-    'name': 'xander',
-    'id': 'u:pnxk9angp3k5knp5',
-    'receivingLanguage': 'English',
-    'sendingLanguage': 'English'
-  },
-  {
-    'name': 'natalie',
-    'id': 'u:qb5lqogelb5eeo0i',
-    'receivingLanguage': 'French',
-    'sendingLanguage': 'French'
-  },
-  {
-    'name': 'sharon',
-    'id': 'u:yfrfzz8wwr8r8tmy',
-    'receivingLanguage': 'Cantonese',
-    'sendingLanguage': 'Cantonese'
-  },
-  {
-    'name': 'ashwin',
-    'id': '',
-    'receivingLanguage': 'English',
-    'sendingLanguage': 'English'
-  },
-]
+var bodyParser = require('body-parser')
 
 flock.setAppId(config.appId);
 flock.setAppSecret(config.appSecret);
 
 var app = express();
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/script'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Listen for events on /events, and verify event tokens using the token verifier.
 app.use(flock.events.tokenVerifier);
 app.post('/events', flock.events.listener);
 
 app.get('/app_launcher', function (event, res) {
-  console.log('*' * 50)
   console.log(event.query.flockEvent)
-  console.log('query:' * 2, event.query)
-  // fs.readFile('side-widget.html',function (err, data){
-  //   res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-  //   res.write(data);
-  //   res.end();
+  // {"userName":"Xander Peterson","userId":"u:pnxk9angp3k5knp5",
+  // "name":"client.pressButton","button":"appLauncherButton"}
+
+  // translate('Ik spreek Engels', {from: 'nl', to: 'en'}).then(res => {
+  //     console.log(res.text);
+  // }).catch(err => {
+  //     console.error(err);
   // });
+
+  res.sendFile(__dirname + '/views/side-widget.html')
+  // res.sendFile('./views/side-widget')
+});
+
+const translate = require('google-translate-api');
+
+app.post('/post_message', function (req, res) {
+  console.log(req.body)
+  return res.json({'hello world': 'asdas'})
+})
+
+//currently sends message to people
+flock.events.on('client.messageAction', function(event, callback){
+    for (var i = 0; i < users.length; i++) {
+      flock.callMethod('chat.sendMessage', tokens['u:pnxk9angp3k5knp5'], {
+          to: users[i]['id'],
+          text: 'hello people!'
+      }, function (error, response) {
+          if (!error) {
+              console.log(response);
+          }
+      });
+    }
   // if (event.query.flockEvent.chat == 'g:107053_lobby') {
-  //   for (var i = 0; i < users.length; i++) {
-  //     flock.callMethod('chat.sendMessage', tokens['u:pnxk9angp3k5knp5'], {
-  //         to: users[i]['id'],
-  //         text: 'hello people!'
-  //     }, function (error, response) {
-  //         if (!error) {
-  //             console.log(response);
-  //         }
-  //     });
-  //   }
   // }
   // else {
   //   flock.callMethod('chat.sendMessage', tokens[event.query.flockEvent.userId], {
@@ -70,33 +64,6 @@ app.get('/app_launcher', function (event, res) {
   //         }
   //     });
   // }
-});
-
-
-//currently sends message to people
-flock.events.on('client.messageAction', function(event, callback){
-  if (event.query.flockEvent.chat == 'g:107053_lobby') {
-    for (var i = 0; i < users.length; i++) {
-      flock.callMethod('chat.sendMessage', tokens['u:pnxk9angp3k5knp5'], {
-          to: users[i]['id'],
-          text: 'hello people!'
-      }, function (error, response) {
-          if (!error) {
-              console.log(response);
-          }
-      });
-    }
-  }
-  else {
-    flock.callMethod('chat.sendMessage', tokens[event.query.flockEvent.userId], {
-          to: 'g:107053_lobby',
-          text: 'hello lobby!'
-      }, function (error, response) {
-          if (!error) {
-              console.log(response);
-          }
-      });
-  }
 });
 
 // Read tokens from a local file, if possible.
@@ -129,3 +96,30 @@ process.on('SIGTERM', process.exit);
 process.on('exit', function () {
     fs.writeFileSync('./tokens.json', JSON.stringify(tokens));
 });
+
+var users = [
+  {
+    'name': 'xander',
+    'id': 'u:pnxk9angp3k5knp5',
+    'receivingLanguage': 'English',
+    'sendingLanguage': 'English'
+  },
+  {
+    'name': 'natalie',
+    'id': 'u:qb5lqogelb5eeo0i',
+    'receivingLanguage': 'French',
+    'sendingLanguage': 'French'
+  },
+  {
+    'name': 'sharon',
+    'id': 'u:yfrfzz8wwr8r8tmy',
+    'receivingLanguage': 'Cantonese',
+    'sendingLanguage': 'Cantonese'
+  },
+  {
+    'name': 'ashwin',
+    'id': 'u:m8oqmjquzhjmh088',
+    'receivingLanguage': 'Tamil',
+    'sendingLanguage': 'Tamil'
+  },
+]
